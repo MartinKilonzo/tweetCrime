b0295369ed7e1551b1ab17df6040392918d8590f
MartinKilonzo <martinkilonzo2@gmail.com>
2017-02-20 23:37:21 -0500

    Fetches past tweets for each live tweet's author.

--- tweetStream.py
+++ tweetStream.py
@@ -5,23 +5,35 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 from credentials import access_token, access_token_secret, consumer_key, consumer_secret
+from userTweets import UserTweets
 
 
 
 class TweetListener(StreamListener):
 
-    def __init__(self, fileStream, tweetFields, tweetTarget=float('inf')):
+    def __init__(self, fileStream, tweetFields, userList=None, tweetTarget=float('inf')):
         self.fileStream = fileStream
         self.tweetFields = tweetFields
+        self.userList = userList
         self.bytesWritten = float(0)
         self.numTweets = 0
         self.tweetTarget = tweetTarget
 
 
+    def _writeTweet(self, tweet, fileStream=None):
+        if fileStream is None:
+            fileStream = self.fileStream
+
+        json.dump(tweet, fileStream)
+        fileStream.write('\n')
+        self.bytesWritten += len(tweet)
+        self.numTweets += 1
+
+
     def on_data(self, data):
         # If the filestream was not initialized, print to stdout
         if self.fileStream == None:
-            print data
+            print '[TweetStream] {:s}'.format(str(data))
             return True
 
         # If the target number of tweets has not been reached, continue to collect tweets
@@ -33,24 +45,39 @@ class TweetListener(StreamListener):
             for field in self.tweetFields:
                 tweet[field] = data[field]
 
-            json.dump(tweet, self.fileStream)
-            self.fileStream.write('\n')
+            self._writeTweet(tweet)
 
-            self.bytesWritten += len(str(tweet))
-            self.numTweets += 1
-            if self.numTweets % 10 == 0:
-                print 'Wrote {:d} tweets comprising {:.2f} megabytes to file.'.format(self.numTweets, self.bytesWritten/1024/1024)
+            user = tweet['user']['screen_name']
+            userTweets = []
+            # If this user's tweets have not already been fetched
+            if user not in self.userList:
+                # Add the user to the user list
+                self.userList.append(user)
+
+                # Get the user's tweets
+                with open('./tweets/userTweets.json', 'a') as userTweetFileStream:
+                    userTweets = UserTweets(user).getTweets(True)
+                    for tweet in userTweets:
+                        self._writeTweet(tweet, userTweetFileStream)
+
+                # Save the user as beeing processed
+                with open('./tweets/users.json', 'a') as userFileStream:
+                    json.dump(user, userFileStream)
+                    userFileStream.write('\n')
+
+            self.bytesWritten += len(str(tweet)) * len(userTweets)
+            print '[TweetStream] Wrote {:d} tweets comprising {:.2f} megabytes to file.'.format(self.numTweets, self.bytesWritten/1024/1024)
             return True
 
         # If the target tweets has been reached, close off the JSON array, and end the stream
         else:
             self.fileStream.close()
-            print 'Finished. Wrote {:d} tweets comprising {:.2f} megabytes to file.'.format(self.numTweets, self.bytesWritten/1024/1024)
+            print '[TweetStream] Finished. Wrote {:d} tweets comprising {:.2f} megabytes to file.'.format(self.numTweets, self.bytesWritten/1024/1024)
             return False
 
 
     def on_error(self, status):
-        print status
+        print '[TweetStream] {:s}'.format(str(status))
         if status == 420:
             self.fileStream.close()
             return False
@@ -59,12 +86,19 @@ class TweetListener(StreamListener):
 
 if __name__ == '__main__':
 
-    with open('./tweets/Template.json') as fs:
+    with open('./tweets/Template.json', 'r') as fs:
         tweetKeys = json.load(fs).keys()
+
+    ul = []
+    with open('./tweets/users.json', 'r') as fs:
+        for line in fs:
+            ul.append(json.loads(line))
+
+    print '[TweetStream] User List: {:s}'.format(str(ul))
 
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    with open('./tweets/vanTweets.json', 'a') as fs:
-        listener = TweetListener(fs, tweetKeys)
+    with open('./tweets/vanTweets2.json', 'a') as fs:
+        listener = TweetListener(fs, tweetKeys, ul)
         stream = Stream(auth, listener)
         stream.filter(locations=[-123.22474, 49.198177, -123.023068, 49.317294])
--- userTweets.py
+++ userTweets.py
@@ -5,36 +5,40 @@ from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import Stream
 
+from credentials import access_token, access_token_secret, consumer_key, consumer_secret
 
-class Tweets():
 
 
-    def __init__(self, access_token, access_token_secret, consumer_key, consumer_secret):
+class UserTweets():
+
+
+    def __init__(self, screenName):
         auth = OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
         self.api = API(auth)
+        self.screenName = screenName
 
 
-    def getTweetsFromUser(self, screenName):
+    def getTweets(self, verbose=False):
         # This method will return the most recent 3240 tweets from the user
         tweets = []
         lastTweet = None
-        newTweets = self.api.user_timeline(screen_name=screenName, count=200, max_id=lastTweet)
+        newTweets = self.api.user_timeline(screen_name=self.screenName, count=200, max_id=lastTweet)
         while len(newTweets) > 0:
             lastTweet = newTweets[-1].id - 1
             tweets.extend([tweet._json for tweet in newTweets])
-            newTweets =self.api.user_timeline(screen_name=screenName, count=200, max_id=lastTweet)
-            print '[Tweet] Added {:d} tweets from {:s}.'.format(len(tweets), screenName)
+            newTweets = self.api.user_timeline(screen_name=self.screenName, count=200, max_id=lastTweet)
+        if verbose:
+            print '[UserTweets] Added {:d} tweets from {:s}.'.format(len(tweets), self.screenName)
 
         return tweets
 
 
 
 if __name__ == '__main__':
-    from credentials import access_token, access_token_secret, consumer_key, consumer_secret
 
     user = 'realDonaldTrump'
-    tweetList = Tweets(access_token, access_token_secret, consumer_key, consumer_secret)
+    tweetList = UserTweets(access_token, access_token_secret, consumer_key, consumer_secret)
 
     with open(user + 'Tweets.json', 'w') as fileStream:
         json.dump(tweetList.getTweetsFromUser(user), fileStream, indent = 4)
